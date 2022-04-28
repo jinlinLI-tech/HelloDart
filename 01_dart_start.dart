d6083cd684ca3aef510ae433aecffea0df7eda9b
jinlinLI-tech <2938210727@qq.com>
2022-04-28 01:35:14 +0000

    修改：修复了程序中原来的类错误点

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -91,7 +91,7 @@ class Spacecraft {
   int? get launchYear => launchDate?.year;
   Spacecraft(this.name, this.launchDate) {}
   Spacecraft.unlaunched(String name) : this(name, null);
-  void describe(){
+  void describe() {
     print('Spacecraft :$name');
     var launchDate = this.launchDate;
     if (launchDate != null) {
@@ -101,13 +101,14 @@ class Spacecraft {
       print('Unlaunched');
     }
   }
-  
 }
+
 class Orbiter extends Spacecraft {
   double altitude;
   Orbiter(String name, DateTime launchDate, this.altitude)
-       :super(name, launchDate);
+      : super(name, launchDate);
 }
+
 //Mixins(https://dart.cn/samples#mixins)
 //简单理解为可以增加代码的复用性
 mixin Piloted {
@@ -117,15 +118,31 @@ mixin Piloted {
     print('Number of astronauts: $astronauts');
   }
 }
-class PilotedCraft extends Spacecraft with Piloted {
 
+class PilotedCraft extends Spacecraft with Piloted {
+  PilotedCraft(String name, DateTime? launchDate) : super(name, launchDate);
 }
+
 //接口和抽象类(https://dart.cn/samples#interfaces-and-abstract-classes)
 //Dart 没有 interface 关键字。相反，所有的类都隐式定义了一个接口。因此，任意类都可以作为接口被实现。用interface和abstract两个关键字
 //继承抽象类和接口时需要重写里面的方法
 class MockSpaceship implements Spacecraft {
+  @override
+  DateTime? launchDate;
 
+  @override
+  late String name;
+
+  @override
+  void describe() {
+    // TODO: implement describe
+  }
+
+  @override
+  // TODO: implement launchYear
+  int? get launchYear => throw UnimplementedError();
 }
+
 abstract class Describable {
   void describe();
   void describeWithEmphasis() {
@@ -134,12 +151,14 @@ abstract class Describable {
     print('=========');
   }
 }
+
 //继承Describable抽象类，重写了describe方法
 class abstractTest extends Describable {
   void describe() {
     print('Hello World!');
   }
 }
+
 //异步（https://dart.cn/samples#async）
 //使用 async 和 await 关键字可以让你避免回调地狱（Callback Hell）并使你的代码更具可读性。
 const oneSecond = Duration(seconds: 1);
@@ -147,6 +166,7 @@ Future<void> printWithDelay(String message) async {
   await Future.delayed(oneSecond);
   print(message);
 }
+
 //异常(https://dart.cn/samples#exceptions)
 /*
  *测试异常
@@ -175,16 +195,13 @@ void main(List<String> args) {
   testFunction(20); //调用斐波那契数列递归函数，测试第20个数的值
   flybyObjects.where((name) => name.contains('turn')).forEach(print); //调用胖箭头函数
   var x = sqrt(100);
-  print('square of 100 is $x');//测试导包是否成功
+  print('square of 100 is $x'); //测试导包是否成功
   var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
   voyager.describe();
   var voyager3 = Spacecraft.unlaunched('Voyager III');
   voyager3.describe();
-  abstractTest test = new abstractTest();//测试抽象类
+  abstractTest test = new abstractTest(); //测试抽象类
   test.describeWithEmphasis();
-  printWithDelay('Hello World!');//测试异步
+  printWithDelay('Hello World!'); //测试异步
   testTry();
-
-
-
 }
